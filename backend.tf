8ec5245459b19aec10716940534645b48005a6f3
diegojlima <dj_lima@live.com>
2025-03-13 22:27:02 -0300

    fix terraform state

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-# ./global-infra/terraform/backend.tf
+# Root directory backend.tf
 
 terraform {
   # Using S3 backend with DynamoDB locking
   backend "s3" {
     bucket         = "delima-aippointment-terraform-state"
-    key            = "global/terraform.tfstate"
+    key            = "root/terraform.tfstate"
     region         = "us-west-2" # Use your preferred region
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
